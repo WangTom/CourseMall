8a05e3735e52d34b8475e6548989a2103460ecc2
741174728@qq.com <741174728@qq.com>
2020-01-02 20:52:02 +0800

    项目初始化

---
+++ src/main/java/com/tom/custinfosrv/CustinfosrvApplication.java
@@ -0,0 +1,13 @@
+package com.tom.custinfosrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustinfosrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustinfosrvApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/tom/custinfosrv/controller/UserInfoController.java
@@ -0,0 +1,4 @@
+package com.tom.custinfosrv.controller;
+
+public class UserInfoController {
+}
